dea670acea0508fe221c6704dd204a974f303177
Dane Lipscombe <dane@lipscombe.com.au>
2018-02-08 13:00:14 +1100

    Add vscode settings

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 set -e
-IGNORE_FILES=(README.md)
+IGNORE_FILES=(README.md vscode)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 
 DRY_RUN=$1
 
 function run
 {
     if [ "$DRY_RUN" == "--dryrun" ]; then
-        echo $1
+        echo "DRY_RUN: $1"
     else
+        echo $1
         eval $1
     fi
     
@@ -30,14 +30,31 @@ function array_contains () {
   return $in
 }
 
+function create_symlink () {
+    FROM=$1
+    TO=$2
+    if [ -e "$TO" ]; then
+        run "rm -rf \"$TO\""
+    fi
+    run "ln -s \"$FROM\" \"$TO\""
+}
+
+#Create symlinks for all files except those in $IGNORE_FILES
 for FILE in $DIR/*; do
     BASE_FILENAME=$(basename $FILE)
     if ( ! array_contains IGNORE_FILES $BASE_FILENAME ); then
-        if [ -e ~/.$BASE_FILENAME ]; then
-            run "rm -rf ~/.$BASE_FILENAME"
-        fi
-        run "ln -s $FILE ~/.$BASE_FILENAME"
+        create_symlink $FILE $HOME/.$BASE_FILENAME 
     fi
 done
 
-
+#OS Specific symlinks
+case $(uname) in
+  'Linux')
+    ;;
+  'WindowsNT')
+    ;;
+  'Darwin') 
+    run "mkdir -p $HOME/Library/Application Support/Code/User"
+    create_symlink $DIR/vscode/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
+    ;;
+esac
