878ea4c1c379fb1cc6ee8d1487309fea11868475
Dane Lipscombe <dane@lipscombe.com.au>
2018-03-10 22:03:31 +1100

    Check brew bundle installed

--- bash/inc/brew.sh
+++ bash/inc/brew.sh
@@ -4,15 +4,20 @@ function brew_install() {
         if ! which brew &> /dev/null; then
             run 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
         fi
-
-        run "brew bundle --no-upgrade --file='$(dotfiles_dir)/nolink/Brewfile'"
+        if ! brew bundle check --no-upgrade --file="$(dotfiles_dir)/nolink/Brewfile" &> /dev/null; then
+            run "brew bundle --no-upgrade --file='$(dotfiles_dir)/nolink/Brewfile'"
+        fi
         ;;
     esac
     
 }
 
+function brew_cleanup() {
+    run "brew bundle cleanup --force --file='$(dotfiles_dir)/nolink/Brewfile'"
+}
+
 function brew_save() {
-    run "rm -f '$(dotfiles_dir)/Brewfile'"
+    run "rm -f '$(dotfiles_dir)/nolink/Brewfile'"
     run "brew bundle dump --file='$(dotfiles_dir)/nolink/Brewfile'"
 }
 
