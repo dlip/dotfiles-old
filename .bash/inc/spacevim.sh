89ddb57245ef5dda2e3f8b5af908811a68f16c8e
Dane Lipscombe <dane@lipscombe.com.au>
2018-03-11 11:28:06 +1100

    Fix indent

--- .bash/inc/spacevim.sh
+++ .bash/inc/spacevim.sh
@@ -1,9 +1,9 @@
 spacevim_install() {
-  if [ ! -d $HOME/.SpaceVim ]; then
-    run 'curl -sLf https://spacevim.org/install.sh | bash'
-  fi
+    if [ ! -d $HOME/.SpaceVim ]; then
+      run 'curl -sLf https://spacevim.org/install.sh | bash'
+    fi
 }
 
 spacevim_update() {
-  run 'cd $HOME/.SpaceVim && git pull'
+    run 'cd $HOME/.SpaceVim && git pull'
 }
