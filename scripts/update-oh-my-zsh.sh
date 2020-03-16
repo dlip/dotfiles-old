12f9735353b2721ad3c8f5ac8bd2b2965277883c
Dane Lipscombe <dane@lipscombe.com.au>
2020-03-16 16:01:07 +1100

    add powerlevel9k

--- scripts/update-oh-my-zsh.sh
+++ scripts/update-oh-my-zsh.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+
 curl -s -L -o oh-my-zsh-master.tar.gz https://github.com/robbyrussell/oh-my-zsh/archive/master.tar.gz
 chezmoi import --strip-components 1 --destination ${HOME}/.oh-my-zsh oh-my-zsh-master.tar.gz
+curl -s -L -o powerlevel-9k.tar.gz https://github.com/Powerlevel9k/powerlevel9k/archive/master.tar.gz
+chezmoi import --strip-components 1 --destination ${HOME}/.oh-my-zsh/custom/themes/powerlevel9k powerlevel-9k.tar.gz
