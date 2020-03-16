d3674f44be3a14cc4f2f02de34bfad6b18cf7125
Dane Lipscombe <dane@lipscombe.com.au>
2020-03-16 15:47:45 +1100

    Add ohmyzsh

---
+++ scripts/update-oh-my-zsh.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+curl -s -L -o oh-my-zsh-master.tar.gz https://github.com/robbyrussell/oh-my-zsh/archive/master.tar.gz
+chezmoi import --strip-components 1 --destination ${HOME}/.oh-my-zsh oh-my-zsh-master.tar.gz
